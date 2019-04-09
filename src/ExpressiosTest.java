8341045a7da39cc13fb22b7e8189be5ba1ced6bf
Guy Shoham <guyshoham28@gmail.com>
2019-04-09 16:11:25 +0300

    ExpressiosTest.java

---
+++ src/ExpressiosTest.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ExpressiosTest {
+
+    private static final double E = 2.71;
+    private static final double PI = Math.PI;
+
+
+    public static void main(String[] args) throws Exception {
+        Map<String, Double> assignment = new TreeMap<>();
+        assignment.put("x", 2.0);
+        assignment.put("y", 0.25);
+        assignment.put("e", 2.71);
+        Expression e, de;
+
+        //(2x) + (sin(4y)) + (e^x)
+        e = new Plus(new Plus(new Mult(2, "x"), new Sin(new Mult(4, "y"))), new Pow("e", "x"));
+        System.out.println(e);
+        System.out.println(e.evaluate(assignment));
+        de = e.differentiate("x");
+        System.out.println(de);
+        System.out.println(de.evaluate(assignment));
+        System.out.println(de.simplify());
+    }
+
+}
